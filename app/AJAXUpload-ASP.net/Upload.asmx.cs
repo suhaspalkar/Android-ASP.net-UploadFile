35fe2c3eaadd9fbbdc2b18ffe8feb44e3987cab6
Suhas Palkar <suhas.g.palkar@gmail.com>
2014-01-20 17:58:01 +0530

    Configure repository

---
+++ app/AJAXUpload-ASP.net/Upload.asmx.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Web;
+using System.Web.Services;
+using System.Xml;
+
+namespace AJAXUpload
+{
+    /// <summary>
+    /// Summary description for Upload.
+    /// </summary>
+    public class Upload : System.Web.Services.WebService
+    {
+        public Upload()
+        {
+            //CODEGEN: This call is required by the ASP.NET Web Services Designer
+            InitializeComponent();
+        }
+        const int InitialBufferLength = 8192;
+
+        #region Component Designer generated code
+
+        //Required by the Web Services Designer 
+        private IContainer components = null;
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+        }
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && components != null)
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #endregion
+
+        [WebMethod]
+        public XmlDocument UploadData(string fileName, int fileSize, byte[] file)
+        {
+            if (fileName == null || fileName == string.Empty || file == null)
+                return GetXmlDocument(Guid.NewGuid(), "Incorrect UploadData Request", 0, 0);
+
+            string filePath = GetFilePath(fileName);
+
+            long offset = 0;
+            using (FileStream fs = File.Open(filePath, FileMode.Append))
+            {
+                fs.Write(file, 0, file.Length);
+                offset = fs.Length;
+            }
+            return GetXmlDocument(Guid.NewGuid(), string.Empty, offset, (InitialBufferLength + offset) > fileSize ? (int)(fileSize - offset) : InitialBufferLength);
+        }
+
+        [WebMethod]
+        public XmlDocument InitUpload(int fileSize, string fileName, bool overwriteFile)
+        {
+            long offset = 0;
+            string filePath = GetFilePath(fileName);
+
+            if (File.Exists(filePath))
+            {
+                if (overwriteFile)
+                {
+                    File.Delete(filePath);
+                }
+                else
+                {
+                    using (FileStream fs = File.Open(filePath, FileMode.Append))
+                    {
+                        offset = fs.Length;
+                    }
+                }
+            }
+
+            return GetXmlDocument(Guid.NewGuid(), string.Empty, offset, (InitialBufferLength + offset) > fileSize ? (int)(fileSize - offset) : InitialBufferLength);
+        }
+
+        private string GetFilePath(string fileName)
+        {
+            string uploadFolder = Path.Combine(this.Context.ApplicationInstance.Request.PhysicalApplicationPath, "Upload");
+            return Path.Combine(uploadFolder, fileName);
+        }
+        private XmlDocument GetXmlDocument(Guid id, string errorMessage, long offset, int bufferLength)
+        {
+            XmlDocument xml = new XmlDocument();
+            MemoryStream memoryStream = new MemoryStream();
+
+            XmlTextWriter writer = new XmlTextWriter(memoryStream, Encoding.UTF8);
+            writer.WriteStartElement("UploadResponse");
+            writer.WriteElementString("ID", id.ToString());
+            writer.WriteElementString("ErrorMessage", errorMessage);
+            writer.WriteElementString("OffSet", offset.ToString());
+            writer.WriteElementString("BufferLength", bufferLength.ToString());
+            writer.WriteEndElement();
+            writer.Flush();
+
+            writer.BaseStream.Position = 0;
+            xml.Load(writer.BaseStream);
+            writer.Close();
+
+            return xml;
+        }
+    }
+}
---
+++ app/src/main/java/com/exp/app/MainActivity.java
@@ -0,0 +1,176 @@
+package com.exp.app;
+
+import android.annotation.TargetApi;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.os.StrictMode;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+
+public class MainActivity extends ActionBarActivity {
+
+    @TargetApi(Build.VERSION_CODES.GINGERBREAD)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+
+        // most important thread policy to get output stream @ line no. 85 i.e.  outputStream = new DataOutputStream(connection.getOutputStream());
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+
+    }
+
+
+
+    @TargetApi(Build.VERSION_CODES.FROYO)
+    public static void sendFile() {       // send large file in multiparts
+        HttpURLConnection connection = null;
+        DataOutputStream outputStream = null;
+
+        String fileName = "img1.jpg"; //any file which is available on device
+        String pathToOurFile = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES) + "/" + fileName;
+        String urlServer = "http://192.168.10.232/AJAXUpload/uploadify.ashx"; // Your server URL (ashx http handler)
+
+        String lineEnd = "\r\n";
+        String twoHyphens = "--";
+        String boundary = "*****";
+
+        int bytesRead, bytesAvailable, bufferSize;
+        byte[] buffer;
+        int maxBufferSize = 1 * 1024 * 1024;
+
+        try {
+            FileInputStream fileInputStream = new FileInputStream(new File(pathToOurFile));
+
+            URL url = new URL(urlServer);
+
+            connection = (HttpURLConnection) url.openConnection();
+
+            // Allow Inputs & Outputs
+            connection.setDoInput(true);
+            connection.setDoOutput(true);
+            connection.setUseCaches(false);
+
+            // Enable POST method
+            connection.setRequestMethod("POST");
+
+            connection.setRequestProperty("Connection", "Keep-Alive");
+            connection.setRequestProperty("Content-Type", "multipart/form-data;boundary=" + boundary);
+
+            outputStream = new DataOutputStream(connection.getOutputStream());
+            outputStream.writeBytes(twoHyphens + boundary + lineEnd);
+            outputStream.writeBytes("Content-Disposition: form-data; name=\"Filedata\";filename=\"" + fileName + "\"" + lineEnd); // form parameters :
+            outputStream.writeBytes(lineEnd);
+
+            bytesAvailable = fileInputStream.available();
+            bufferSize = Math.min(bytesAvailable, maxBufferSize);
+            buffer = new byte[bufferSize];
+
+            // Read file
+            bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+
+            while (bytesRead > 0) {
+                outputStream.write(buffer, 0, bufferSize);
+                bytesAvailable = fileInputStream.available();
+                bufferSize = Math.min(bytesAvailable, maxBufferSize);
+                bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+            }
+
+            outputStream.writeBytes(lineEnd);
+            outputStream.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
+
+            // Responses from the server (code and message)
+            // do not delete/comment the following two lines.
+            int serverResponseCode = connection.getResponseCode();
+            String serverResponseMessage = connection.getResponseMessage();
+
+            fileInputStream.close();
+            outputStream.flush();
+            outputStream.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (ProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+
+            rootView.findViewById(R.id.uploadButton).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    //Inform the user the button has been clicked
+                    sendFile();
+                }
+            });
+            return rootView;
+        }
+    }
+
+
+
+}
